b7504a028b4f3702e3a22cbf31790cd685df8de6
murty <murtykorada@murtykorada-Lenovo-Z480.(none)>
2015-04-19 12:53:10 -0400

    unicorn rb log

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,22 +5,24 @@ timeout Integer(ENV['WEB_TIMEOUT'] || 15)
 preload_app true
 
 before_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
+  if defined? ActiveRecord::Base
     ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
 end
 
 after_fork do |server, worker|
-
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
+  if defined? ActiveRecord::Base
     ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
 end
