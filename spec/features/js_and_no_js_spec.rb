cfa71d39a08a01820eb3d534acf2b939effb9a03
murty <murtykorada@murtykorada-Lenovo-Z480.(none)>
2015-05-05 22:36:34 -0400

    trying login path

--- spec/features/js_and_no_js_spec.rb
+++ spec/features/js_and_no_js_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 describe 'signin page' do
   it 'no js' do
-    visit root_path
+    visit '/login'
     expect(page).to have_text 'Remember Me'
   end
   it 'with js', js: true do
-    visit root_path
+    visit '/login'
     expect(page).to have_text 'Remember Me'
   end
 end
