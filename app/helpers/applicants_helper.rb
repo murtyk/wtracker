9ef11c7f1920470571fe99ebce03b9538d1581de
murty <murtykorada@murtykorada-Lenovo-Z480.(none)>
2015-10-11 07:02:37 -0400

    serves specific js assess for index action

--- app/helpers/applicants_helper.rb
+++ app/helpers/applicants_helper.rb
@@ -27,33 +27,4 @@ module ApplicantsHelper
       .group('sectors.id', 'sectors.name')
       .count
   end
-
-  def asset_exists?(filename, extension)
-    asset_pathname = "#{Rails.root}/app/assets/"
-    asset_file_path = "#{asset_pathname}/#{filename}".split('.')[0]
-    !Dir.glob("#{asset_file_path}.#{extension}*").empty?
-  end
-
-  def js_asset_exists?(filename)
-    asset_exists?("javascripts/#{filename}", 'js')
-  end
-
-  def css_asset_exists?(filename)
-    asset_exists?("stylesheets/#{filename}", 'css')
-  end
-
-  def js_asset_name(c_name, a_name)
-    asset_name = "#{c_name}/#{a_name}"
-    return asset_name if js_asset_exists?(asset_name)
-
-    return nil unless a_name == 'index'
-
-    js_index_asset(c_name)
-  end
-
-  def js_index_asset(c_name)
-    asset_name = "#{c_name}/indirect_index"
-
-    js_asset_exists?(asset_name) ? asset_name : nil
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,31 @@
 # helper methods
 module ApplicationHelper
+  def asset_exists?(filename, extension)
+    asset_pathname = "#{Rails.root}/app/assets/"
+    asset_file_path = "#{asset_pathname}/#{filename}".split('.')[0]
+    !Dir.glob("#{asset_file_path}.#{extension}*").empty?
+  end
+
+  def js_asset_exists?(filename)
+    asset_exists?("javascripts/#{filename}", 'js')
+  end
+
+  def css_asset_exists?(filename)
+    asset_exists?("stylesheets/#{filename}", 'css')
+  end
+
+  def page_specific_js_asset(c_name, a_name)
+    return index_page_js_asset(c_name) if a_name == 'index'
+
+    asset_name = "#{c_name}/#{a_name}"
+    js_asset_exists?(asset_name) ? asset_name : nil
+  end
+
+  def index_page_js_asset(c_name)
+    asset_name = "#{c_name}/indirect_index"
+    return asset_name if js_asset_exists?(asset_name)
+
+    asset_name = "#{c_name}/index"
+    js_asset_exists?(asset_name) ? asset_name : nil
+  end
 end
