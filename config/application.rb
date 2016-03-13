75306fbe9dda05130271cb36d760284b96c6ec87
murtyk <murtyk@yahoo.com>
2016-03-13 17:23:00 -0400

    local env vars through a file

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,14 @@ module WTracker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value.to_s
+      end if File.exists?(env_file)
+    end
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
