d7637e44b4400ab49652cbde39db70b83f8dc2b4
Murty Korada <murty.korada@vydia.com>
2016-05-09 13:19:22 -0400

    damn

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -102,7 +102,7 @@ class UserMailer < ActionMailer::Base
     docs.each { |k, v| attachments[k] = v } if docs
     mail(to: user.email,
          subject: subject,
-         from: "Support<ENV['SUPPORT_FROM_EMAIL']>") do |format|
+         from: "Support<#{ENV['SUPPORT_FROM_EMAIL']}>") do |format|
            format.html { render inline: body }
          end
   end
