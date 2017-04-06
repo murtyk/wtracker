1084c14f9eb289504d2b15b1b38873273331f846
Murty Korada <murtyk@yahoo.com>
2017-04-06 17:38:40 -0400

    fixed specs

--- spec/features/trainee_services_ajax_spec.rb
+++ spec/features/trainee_services_ajax_spec.rb
@@ -13,28 +13,30 @@ describe 'TraineeService', js: true do
     it 'user adds, updates and deleted a Service' do
       click_link "trainee_#{get_trainee_ids[0]}_new_trainee_service_link"
       wait_for_ajax
-      
+
       fill_in 'trainee_service_name', with: 'Sahu Service'
       fill_in 'trainee_service_start_date', with: '11/01/2017'
-            
-      click_on 'Add'
       wait_for_ajax
+
+      find_button('Add').trigger("click")
+      wait_for_ajax
+
       expect(page).to have_text 'Sahu Service'
 
       click_link "edit_trainee_service_#{get_trainee_services_ids[0]}_link"
       wait_for_ajax
-      
+
       fill_in 'trainee_service_name', with: 'Sahu Service updated'
       fill_in 'trainee_service_start_date', with: '11/01/2017'
-           
+
       click_on 'Update'
       wait_for_ajax
       expect(page).to have_text 'Sahu Service updated'
 
       AlertConfirmer.accept_confirm_from do
         click_link "destroy_trainee_service_#{get_trainee_services_ids[0]}_link"
-
       end
+
       wait_for_ajax
       expect(page).to_not have_text 'Sahu Service'
     end
