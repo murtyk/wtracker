cf155595dd4a14a63ec1cddb4380d4d4a733d0ec
Murty Korada <murtyk@yahoo.com>
2017-09-20 23:39:54 -0400

    report profiling

--- app/reports/report_data.rb
+++ app/reports/report_data.rb
@@ -10,7 +10,9 @@ class ReportData
   end
 
   def send_to_user
+    log_info("preparing report #{params[:report_name]} for user #{user.email} ")
     excel_file.send_to_user
+    log_info("sent report #{params[:report_name]} to #{user.email} ")
   end
 
   def excel_file
@@ -22,5 +24,9 @@ class ReportData
     Account.current_id = account_id
     Grant.current_id = grant_id
     User.find user_id
+  end
+
+  def log_info(msg)
+    Rails.logger.info "ReportData: #{Time.now}  " +  msg
   end
 end
