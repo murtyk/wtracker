a6b64e43cd416565a64e772068aa0cd725ac7574
Murty Korada <murtyk@yahoo.com>
2018-01-02 18:11:34 -0500

    drop demo column in accounts tables since we have options

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101033726) do
+ActiveRecord::Schema.define(version: 20180102230713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20180101033726) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.text     "options"
-    t.boolean  "demo"
   end
 
   create_table "addresses", force: :cascade do |t|
