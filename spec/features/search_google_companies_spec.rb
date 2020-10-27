5b66f3688d3d8193a2addb2c927802e380cd07e8
Murty Korada <murtyk@yahoo.com>
2020-10-26 20:35:03 -0400

    Update search_google_companies_spec.rb

--- spec/features/search_google_companies_spec.rb
+++ spec/features/search_google_companies_spec.rb
@@ -8,11 +8,12 @@ describe 'Google Companies' do
 
     it 'finds companies' do
       visit('/google_companies')
-      VCR.use_cassette('companies_google_search') do
+      VCR.use_cassette('companies_google_search', record: :none) do
         fill_in 'filters_name', with: 'Munich Re'
         fill_in 'filters_location', with: 'Princeton, NJ'
         click_on 'Find'
-        expect(page).to have_text 'Munich Reinsurance America Inc.'
+
+        expect(page).to have_text 'Munich Reinsurance America Inc'
       end
     end
   end
