988526dc291d8b5f38ebde9a8138a9bbe9e70f95
Murty Korada <murtyk@yahoo.com>
2023-04-09 09:55:50 -0400

    disable activemodel serializer classes

--- app/serializers/admin_serializer.rb
+++ app/serializers/admin_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class AdminSerializer < ActiveModel::Serializer
-  attributes :email, :auth_token
-end
+# class AdminSerializer < ActiveModel::Serializer
+#   attributes :email, :auth_token
+# end
--- app/serializers/leads_queue_serializer.rb
+++ app/serializers/leads_queue_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class LeadsQueueSerializer < ActiveModel::Serializer
-  attributes :id, :trainee_id, :status, :data
-end
+# class LeadsQueueSerializer < ActiveModel::Serializer
+#   attributes :id, :trainee_id, :status, :data
+# end
