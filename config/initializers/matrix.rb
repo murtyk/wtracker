00efd15eeb0503cdc9414b264b4ebc258bfd0156
Murty Korada <murtyk@yahoo.com>
2023-04-12 21:21:59 -0400

    Update matrix.rb

--- config/initializers/matrix.rb
+++ config/initializers/matrix.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'matrix'
-class Matrix
-  public :"[]=", :set_element, :set_component
-end
+# require 'matrix'
+# class Matrix
+#   public :"[]=", :set_element, :set_component
+# end
